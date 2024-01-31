d08c7a7ef55b6cdb4c04dc65bc3eb62fecb12243
DimaD1988 <dimdon88@gmail.com>
2024-01-31 16:19:18 +0300

    feat: Components HeroSection init files

---
+++ Components/06 hero-section/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	content: ['./*.html'],
+	theme: {
+		fontFamily: {
+			serif: ['Crimson Text', 'serif'],
+			sans: ['DM Sans', 'sans-serif'],
+		},
+		extend: {
+			colors: {
+				herowine: '#920038',
+				herolight: '#FF7EB5',
+			},
+		},
+	},
+	plugins: [],
+};
